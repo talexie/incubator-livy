f492e3ea4b9467ef3af3f0665427d5ef19332413
Alex Tumwesigye <atumwesigye@gmail.com>
2023-04-19 22:03:54 +0300

    Fix docker build for spark 3.x+

--- dev/docker/start_server.sh
+++ dev/docker/start_server.sh
@@ -3,6 +3,7 @@
 export SPARK_HOME=/opt/spark
 
 echo "Starting the Server"
+/opt/livy/server/bin/livy-server stop
 /opt/livy/server/bin/livy-server start
 echo "Logging started."
 tail -f /opt/livy/server/logs/livy--server.out
